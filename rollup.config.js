af27a38c01cd59b75a0f55be860310295892d8e3
switch-900 <switchnine900@gmail.com>
2025-06-28 12:02:27 +0100

    modified:   rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default defineConfig({
